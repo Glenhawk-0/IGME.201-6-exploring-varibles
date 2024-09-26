1fc5d01b544134c1973d334781ff9624ffc2ac36
Glenhawk-0 <LennonEhare@Gmail.com>
2024-09-26 08:52:27 -0400

    added stats

--- ExploringVaribles/ExploringVaribles/Program.cs
+++ ExploringVaribles/ExploringVaribles/Program.cs
@@ -14,6 +14,16 @@
             string minion1Name = "401";
             string minion2Name = "402";
 
+
+            //stats
+            int heroHealth = 100;
+            int heroStrength = 50;
+            int bossHealth = 100;
+            int bossStrength = 25;
+            int minion1Health = 50;
+            int minion2Health = 50;
+            int minionStrength = 5;
+
             //begin Code Instructions
 
             Console.WriteLine("Behold my fellow code's, our saviour has arrived. " + "our hero's name is " + heroFullName + "!!!");
@@ -21,6 +31,13 @@
             Console.WriteLine(heroFullName + "'s arch nemesis is " + villianName + " the " + villianTitle);
             Console.WriteLine(villianName + " the " + villianTitle + " commands the " + minionTitle + " such as the monstrous " + minion1Name + " and the terrible " + minion2Name + "!");
 
+            //the fight
+            Console.WriteLine("the fight shall commence");
+            Console.WriteLine("hero's health: " + heroHealth);
+            Console.WriteLine("boss's health: " + bossHealth);
+            Console.WriteLine("minion 1's health: " + minion1Health);
+            Console.WriteLine("minion 2's health: " + minion2Health);
+            
         }
     }
 }
