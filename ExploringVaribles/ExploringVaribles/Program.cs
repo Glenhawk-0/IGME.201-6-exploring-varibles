0a81704eeae4ee37676d7cf3c12554449a67e57d
Glenhawk-0 <LennonEhare@Gmail.com>
2024-09-26 08:26:26 -0400

    added a glorious message
    our hero arrived

--- ExploringVaribles/ExploringVaribles/Program.cs
+++ ExploringVaribles/ExploringVaribles/Program.cs
@@ -7,8 +7,14 @@
             //varibles
             string heroFirstName = "Lennon";
             string heroLastName =   "Hare";
-            string heroFullName = heroFirstName + heroLastName;
+            string heroFullName = heroFirstName +" "+ heroLastName;
 
-                }
+
+            //begin Code Instructions
+
+            Console.WriteLine("Behold my fellow code's our saviour has arrived. " + "our hero's name is " + heroFullName + "!!!");
+
+
+        }
     }
 }
