5a0c45999d521a6b821142891b528655c9faa54d
Glenhawk-0 <LennonEhare@Gmail.com>
2024-09-26 08:21:10 -0400

    added our hero's first, last and full name
    how great!!!

---
+++ ExploringVaribles/ExploringVaribles/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExploringVaribles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //varibles
+            string heroFirstName = "Lennon";
+            string heroLastName =   "Hare";
+            string heroFullName = heroFirstName + heroLastName;
+
+                }
+    }
+}
