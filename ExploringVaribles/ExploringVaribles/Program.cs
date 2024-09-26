3f101c59c7fc3d17457d161906d1ed0aa1bc5d11
Glenhawk-0 <LennonEhare@Gmail.com>
2024-09-26 08:45:06 -0400

    we have our villians
    oh no

--- ExploringVaribles/ExploringVaribles/Program.cs
+++ ExploringVaribles/ExploringVaribles/Program.cs
@@ -8,12 +8,18 @@
             string heroFirstName = "Lennon";
             string heroLastName =   "Hare";
             string heroFullName = heroFirstName +" "+ heroLastName;
-
+            string villianTitle = "Lord of Server Bug's";
+            string minionTitle = "Bug's";
+            string villianName = "Craig";
+            string minion1Name = "401";
+            string minion2Name = "402";
 
             //begin Code Instructions
 
-            Console.WriteLine("Behold my fellow code's our saviour has arrived. " + "our hero's name is " + heroFullName + "!!!");
+            Console.WriteLine("Behold my fellow code's, our saviour has arrived. " + "our hero's name is " + heroFullName + "!!!");
 
+            Console.WriteLine(heroFullName + "'s arch nemesis is " + villianName + " the " + villianTitle);
+            Console.WriteLine(villianName + " the " + villianTitle + " commands the " + minionTitle + " such as the monstrous " + minion1Name + " and the terrible " + minion2Name + "!");
 
         }
     }
